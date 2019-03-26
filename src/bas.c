86622cb211dfbc2f9550035fb9e26e20f7905bc4
altnova <arina@lateralsummer.com>
2019-03-26 19:19:39 +0300

    [~]

--- src/bas.c
+++ src/bas.c
@@ -1,16 +1,24 @@
+//< main loop
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "___.h"
+#include "cfg.h"
 
+#include "alg/txt.h"
+#include "alg/set.h"
 
-
-
+//< TODO
+//<	hsh_print()
 
 
 I main()
 {
-	FILE *f  = fopen_();
-	P(f == NULL, 1);
-	P(make_stop_trie() == NIL, 1);
-	R process();
+	FILE *f;
+	P(set_start(f) == NIL, 1);					//< make adt and open files
+	X(txt_process(f) == NIL, set_end(f), 1);	//< process the text 
+	X(hsh_print() == NIL, set_end(f), 1);		//< print results
+	set_end(f);									//<	clean
+	R0;
 }
