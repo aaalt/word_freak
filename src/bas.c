a91fa6b3382cc7bc0d732c26cc1db8ed8f0d9696
altnova <arina@lateralsummer.com>
2019-03-30 20:38:26 +0300

    [~]

--- src/bas.c
+++ src/bas.c
@@ -1,4 +1,4 @@
-//< main loop
+//< main of word_freak
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,14 +10,8 @@
 #include "alg/set.h"
 #include "alg/str.h"
 
-//< TODO
-//<	fopen_()
-
 TRIE STOP_TRIE;
 HT TEXT_HSH;
-
-// #define LEN_ 5000
-// #define _LEN_ 50
 
 C TEXT_BUF_[SZ_TBUF];
 C WORD_BUF_[SZ_WBUF];
@@ -29,11 +23,11 @@ FILE* f_ptr;
 
 I main()
 {
-	// FILE *f;
-	P(set_start(f_ptr) == NIL, 1);					//< make adt and open files
-	O("%d!\n", f_ptr);
-	X(txt_process(f_ptr) == NIL, set_end(f_ptr), 1);	//< process the text 
+	f_ptr = set_start(f_ptr);
+	P((UJ)f_ptr == NIL, 1);
+
+	X(txt_process(f_ptr) == NIL, set_end(f_ptr), 1);			//< process the text 
 	X(str_hsh_print(TEXT_HSH) == NIL, set_end(f_ptr), 1);		//< print results
-	set_end(f_ptr);									//<	clean
+	set_end(f_ptr);												//<	clean
 	R0;
 }
