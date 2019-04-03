4452146bd3e1b27ac49da0127a1166db2cf09596
altnova <arina@lateralsummer.com>
2019-04-03 18:22:34 +0300

    chr.c/.h is a set of functions for basic char and string actions and indicative fns

---
+++ src/alg/chr.h
@@ -0,0 +1,16 @@
+
+// #pragma once
+
+ext C cs(C c);
+
+ext UJ char_is_ext(C c);
+
+ext C in_alphabet(C c);
+
+ext C valid_key(C c);
+
+ext UJ swipe_buf(S buf, I ptr, I lim);
+
+ext V clean_buf(S buf, I len);
+
+ext UJ sz_buf(S buf, UJ max);
