c8bdc098160d24b9f3a3065b6a8e01e344db7322
altnova <arina@lateralsummer.com>
2019-04-11 22:05:32 +0300

    [+] PRINT_PAR for printing a specific amount of strings

--- src/cfg.h
+++ src/cfg.h
@@ -13,13 +13,14 @@
 #define LOGLEVEL L_INFO	//< set to WARN in prod builds
 #endif
 
-#define PRINT_PAR 0			//<	set 1 if decreasing sort is needed
+#define PRINT_PAR 1			//<	set 0 if increasing sort is needed
+#define PRINT_TOP 10		//<	set NIL if you want to print all	
 
 //<	size of WORD_BUF
 // #define SZ_WBUF 120
-#define SZ_WBUF 102			//<	larger SZ_WBUF 	-->	SLOWER program
+#define SZ_WBUF 128			//<	larger SZ_WBUF 	-->	SLOWER program	(*n^2?)
 //<	size of TEXT_BUF
-#define SZ_TBUF 1000000		//<	larger SZ_TBUF 	--> faster program 
+#define SZ_TBUF 32768*2*2		//<	larger SZ_TBUF 	--> FASTER program (*1/logn?)
 // #define SZ_TBUF 60
 
 //<	size of EXT_KEY (amount of extra keys for parsing, trie and hash)
