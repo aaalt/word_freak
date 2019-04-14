fc2afda9e1c7d408f952eea1dbe369196c324cb7
altnova <arina@lateralsummer.com>
2019-04-14 15:59:38 +0300

    [!] i lied. all fine

--- src/cfg.h
+++ src/cfg.h
@@ -14,18 +14,18 @@
 #endif
 
 #define PRINT_PAR 1			//<	set 0 if increasing sort is needed
-#define PRINT_TOP 10		//<	set NIL if you want to print all	
+#define PRINT_TOP NIL		//<	set NIL if you want to print all	
 
-//!	size of EXT_KEY (amount of extra keys for parsing, trie and hash)
+// !	size of EXT_KEY (amount of extra keys for parsing, trie and hash)
 #define EXT_KEY_AM 1
 // #define EXT_KEY_AM 3
 
 #if 1
 //! live run
-// #define SZ_WBUF 1024
-#define SZ_WBUF 4
-// #define SZ_TBUF 131072
-#define SZ_TBUF 8
+#define SZ_WBUF 1024
+// #define SZ_WBUF 4
+#define SZ_TBUF 131072
+// #define SZ_TBUF 8
 //!	path to file which will be processed into hash table
 // #define TXT_FILE "txt/_tr_.txt"
 #define TXT_FILE "txt/overflow.txt"
