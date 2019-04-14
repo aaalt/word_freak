99c01223103501b9463c1301b9d52b59f3d77efd
altnova <arina@lateralsummer.com>
2019-04-14 20:31:03 +0300

    [!] improve redefinition of SZ_TBUF and SZ_WBUF

--- src/alg/txt.h
+++ src/alg/txt.h
@@ -7,15 +7,14 @@ UJ txt_process(FILE* f, V* struct_1, V* struct_2, WORD_ADD fn);
 UJ SZFILE(FILE* ptr);
 
 #ifndef SZ_TBUF
-#define SZ_TBUF 5000
 
-C TEXT_BUF_[SZ_TBUF];
-S TEXT_BUF = &TEXT_BUF_[0];
-
+#define SZ_TBUF 131072
+TXT_TYPE TEXT_BUF_[SZ_TBUF];
+TXT_T TEXT_BUF = &TEXT_BUF_[0];
 #endif
 #ifndef SZ_WBUF
 
-#define SZ_WBUF 100
-C WORD_BUF_[SZ_WBUF];
-S WORD_BUF = &WORD_BUF_[0];
+#define SZ_WBUF 1024
+TXT_TYPE WORD_BUF_[SZ_WBUF];
+TXT_T WORD_BUF = &WORD_BUF_[0];
 #endif
